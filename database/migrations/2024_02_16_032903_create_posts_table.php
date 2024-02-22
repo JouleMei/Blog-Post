d7ef25d0b0775445daa2cf6d26fdb8f388e0bc8d
JouleMei <jmpc0722@gmail.com>
2024-02-22 13:08:57 +0800

    Update

--- database/migrations/2024_02_16_032903_create_posts_table.php
+++ database/migrations/2024_02_16_032903_create_posts_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('slug')->unique();
             $table->string('title');
             $table->text('excerpt');
             $table->text('body');
